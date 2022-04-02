f33ca4301ca862fa20291293f13a9cfbe1b47a16
Sherpad Ndabambi <sgndabambi@gmail.com>
2022-04-02 21:32:01 +0200

    Added logic

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+let hands = ["✊", "🖐", "✌"];
+let score = 0;
+
+function play(playerHand){
+    let index = Math.floor(Math.random() * 3);
+    let cpuHand = hands[index];
+    let result;
+
+    if(cpuHand === playerHand) result = "Draw";
+    else{
+        switch(playerHand){
+            case '✊':
+                if(cpuHand === '🖐'){
+                    result = "Paper covers rock. CPU wins.";
+                    if(score > 0) score--;
+                }
+                else{
+                    result = "Rock crushes scissors. Player wins.";
+                    score++;
+                }
+                break;
+            case '🖐':
+                if(cpuHand === "✊"){
+                    result = "Paper covers rock. Player wins.";
+                    score++;
+                }
+                else{
+                    result = "Scissors cuts paper. CPU wins.";
+                    if(score > 0) score--;
+                }
+                break;
+            default:
+                if(cpuHand === "✊"){
+                    result = "Rock crushes scissors. CPU wins.";
+                    if(score > 0) score--;
+                }
+                else{
+                    result = "Paper covers rock. Player wins.";
+                    score++;
+                }
+        }
+    }
+
+    document.getElementById("scoreSpan").innerText = score.toString();
+    document.getElementById("resultTable").innerHTML +=
+        "<tr>" +
+            "<td>" + playerHand + "</td>" +
+            "<td>" + cpuHand + "</td>" +
+            "<td>" + result + "</td>" +
+        "</tr>";
+}
